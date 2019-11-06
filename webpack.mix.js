232849d649c5ff09ea9455b995f54b24d68b1ce5
AymanYounes <47554998+AymanYounes@users.noreply.github.com>
2019-11-06 16:40:28 +0200

    mix test

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,43 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+// mix.js('resources/js/app.js', 'public/js')
+//    .sass('resources/sass/app.scss', 'public/css');
+
+mix.styles([
+    'public/css/bootstrap.min.css',
+    'public/css/jquery-ui.min.css',
+    'public/css/animate.css',
+    'public/css/css-plugin-collections.css',
+    'public/css/menuzord-skins/menuzord-boxed.css',
+    'public/css/style-main.css',
+    'public/css/preloader.css',
+    'public/css/custom-bootstrap-margin-padding.css',
+    'public/css/responsive.css',
+    'public/css/revolution-slider/css/settings.css',
+    'public/css/revolution-slider/css/layers.css',
+    'public/css/revolution-slider/css/navigation.css',
+    'public/css/colors/theme-skin-blue.css',
+    'public/css/style.css',
+], 'public/css/build.css');
+
+mix.styles([
+    'public/css/bootstrap.min.css',
+    'public/css/bootstrap-rtl.min.css',
+    'public/css/jquery-ui.min.css',
+    'public/css/animate.css',
+    'public/css/css-plugin-collections.css',
+    'public/css/menuzord-skins/menuzord-boxed.css',
+    'public/css/style-main.css',
+    'public/css/style-main-rtl.css',
+    'public/css/style-main-rtl-extra.css',
+    'public/css/preloader.css',
+    'public/css/custom-bootstrap-margin-padding.css',
+    'public/css/responsive.css',
+    'public/css/revolution-slider/css/settings.css',
+    'public/css/revolution-slider/css/layers.css',
+    'public/css/revolution-slider/css/navigation.css',
+    'public/css/colors/theme-skin-blue.css',
+    'public/css/style.css',
+    'public/css/style-ar.css',
+], 'public/css/build-ar.css');
