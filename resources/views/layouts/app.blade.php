1c36da0caa6afb24f3dd31021aecd89085147a11
AymanYounes <47554998+AymanYounes@users.noreply.github.com>
2019-11-10 15:03:49 +0200

    mix test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,12 +79,12 @@
 <!-- external javascripts -->
 
     <script src="js/jquery-2.2.0.min.js"></script>
-    <script src="js/build.js"></script>
     <script src="js/jquery-ui.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <!-- JS | jquery plugin collection for this theme -->
     <script src="js/jquery-plugin-collection.js"></script>
 
+    <script src="js/build.js"></script>
     @yield('early-scripts')
 <!-- Revolution Slider 5.x SCRIPTS -->
 {{--    <script src="js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>--}}
