f6f1bcdc358c7d97ce13911d82fb50c139a4199a
AymanYounes <47554998+AymanYounes@users.noreply.github.com>
2019-11-11 11:45:35 +0200

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,10 +77,10 @@
 {{--        <link href="css/build.css" rel="stylesheet" type="text/css">--}}
 {{--    @endif--}}
 <!-- external javascripts -->
-    <script src="js/build.js"></script>
-{{--    <script src="js/jquery-2.2.0.min.js"></script>--}}
-{{--    <script src="js/jquery-ui.min.js"></script>--}}
-{{--    <script src="js/bootstrap.min.js"></script>--}}
+{{--    <script src="js/build.js"></script>--}}
+    <script src="js/jquery-2.2.0.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <!-- JS | jquery plugin collection for this theme -->
 {{--    <script src="js/jquery-plugin-collection.js"></script>--}}
     <script src="js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
